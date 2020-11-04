befd0360d9e2c98affc5a30110a80ae66fc48b5b
Terry Moreland <tsmoreland@live.com>
2020-11-04 22:48:53 +0000

    Added Sample Api using ApiKeyAuthentication - provides the typical weather forecast endpoint - app for current api key claims - app manager to fetch new ones

---
+++ Moreland.AspNetCore.SampleApi/Controllers/AppManagerController.cs
@@ -0,0 +1,89 @@
+﻿//
+// Copyright © 2020 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moreland.AspNetCore.ApiKeyAuthentication;
+using Moreland.AspNetCore.ApiKeyAuthentication.Data;
+using Moreland.AspNetCore.SampleApi.Models;
+
+namespace Moreland.AspNetCore.SampleApi.Controllers
+{
+    [ApiController]
+    [Route("AppManager")]
+    public class AppManagerController : ControllerBase
+    {
+        private readonly IApiKeyRepository _apiKeyRepository;
+        private readonly ILogger<AppManagerController> _logger;
+
+        /// <summary>
+        /// Instantiates a new instance of the <see cref="AppManagerController"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// if <paramref name="apiKeyRepository"/> or <paramref name="logger"/> is null
+        /// </exception>
+        public AppManagerController(IApiKeyRepository apiKeyRepository, ILogger<AppManagerController> logger)
+        {
+            _apiKeyRepository = apiKeyRepository ?? throw new ArgumentNullException(nameof(apiKeyRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetAppKey(Guid? id)
+        {
+            if (!id.HasValue || id == Guid.Empty)
+                return BadRequest();
+
+            var app = await _apiKeyRepository.GetByIdAsync(id.Value);
+
+            var newApp = new CreateAppModel {Owner = app.Owner, Roles = app.Roles.ToList()};
+
+            var serialized = JsonSerializer.Serialize(newApp, new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase, IgnoreNullValues = true});
+
+            return app.IsNullorEmpty()
+                ? (ActionResult<string>) NotFound()
+                : Ok(app.Key);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateApp(CreateAppModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var (owner, roles) = model;
+
+            if (string.IsNullOrWhiteSpace(owner))
+                return BadRequest();
+
+            var arrayOfRoles = roles?.ToArray() ?? Array.Empty<string>();
+            
+            // if there were more than a quick sample we'd verify the roles were expected values,
+            // possibly even using an enum to represent valid values
+
+            if (arrayOfRoles?.Any() != true)
+                return BadRequest();
+
+            var app = await _apiKeyRepository.CreateAsync(owner, arrayOfRoles);
+
+            return app.IsNullorEmpty()
+                ? (ActionResult)new StatusCodeResult(500) // error should've been logged in create
+                : Created(new Uri($"/App/{app.Id}"), app.Key);
+        }
+    }
+}
---
+++ Moreland.AspNetCore.SampleApi/Data/TestApiKeys.cs
@@ -0,0 +1,35 @@
+﻿//
+// Copyright © 2020 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using Moreland.AspNetCore.ApiKeyAuthentication;
+
+namespace Moreland.AspNetCore.SampleApi.Data
+{
+    public static class TestApiKeys
+    {
+        public static IEnumerable<ApiKey> Keys
+        {
+            get
+            {
+                yield return new ApiKey(new Guid("D0944969-D442-4183-8F7B-05DFA6915E4B"), "resource1", "B47900BB1C704100B60063D1B84B287F",
+                    new DateTime(2019, 06, 01), new[] {"read", "write"});
+                yield return new ApiKey(new Guid("DF17DF48-EB48-46A7-8566-1E243284BCDB"), "resource1", "BE2E4B48DE6D4277BD17EA52884E380B",
+                    new DateTime(2019, 08, 01), new[] {"read"});
+                yield return new ApiKey(new Guid("CC6EBE75-F2C9-4DE2-82AA-66AFB3B8D492"), "resource1", "0C1CE9B860B242FBA5FDF277E65CCCE4",
+                    new DateTime(2020, 01, 01), new[] {"manage", "create", "read", "update", "delete"});
+            }
+        }
+    }
+}
